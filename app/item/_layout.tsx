fe8e2c031ef6a4cf5c62e5a7b5fe1a4741e18cf9
sambaloyi <sambaloyi2017@gmail.com>
2025-04-04 17:19:27 +0200

    Enhance ItemLayout component with updated Stack screen options for improved navigation experience 🎢✨

--- app/item/_layout.tsx
+++ app/item/_layout.tsx
@@ -2,7 +2,13 @@ import { Stack } from "expo-router";
 
 export default function ItemLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        presentation: "card",
+        animation: "slide_from_right",
+      }}
+    >
       <Stack.Screen
         name="[id]"
         options={{
