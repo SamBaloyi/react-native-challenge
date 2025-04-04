ac28a4dc8f985318065f3b411cc49caaac7fcb58
sambaloyi <sambaloyi2017@gmail.com>
2025-04-05 00:46:03 +0200

    Refactor CreateItemScreen to simplify component structure by removing unused hooks and state management. Clean up code by commenting out unnecessary logic related to item creation and network status handling. Enhance readability and maintainability. ✨🛠️

--- app/item/create.tsx
+++ app/item/create.tsx
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { Stack } from "expo-router";
-import { useItems } from "../../store/hooks/useItems";
-// import { useNetwork } from "../../store/hooks/useNetwork";
-import { ItemFormValues } from "../../types/item";
 import ItemForm from "../../components/ItemForm";
 import { ThemedView } from "../../components/ThemedView";
 
 export default function CreateItemScreen() {
-  const { createItem } = useItems();
-//   const { isConnected } = useNetwork();
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const handleSubmit = async (values: ItemFormValues) => {
-    setIsSubmitting(true);
-    try {
-      await createItem(values);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
 
   return (
     <>
@@ -31,9 +16,6 @@ export default function CreateItemScreen() {
       />
       <ThemedView style={styles.container}>
         <ItemForm
-        //   initialValues={{ title: "", body: "", userId: 1 }}
-        //   onSubmit={handleSubmit}
-        //   isLoading={isSubmitting}
         />
       </ThemedView>
     </>
